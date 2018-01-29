52af4cb0733cb337fc3f88819d52c5536a50767b
Steven R. Loomis <srloomis@us.ibm.com>
2018-01-29 15:22:11 -0800

    updated scripts for subgit

--- scripts/gitfilter.sh
+++ scripts/gitfilter.sh
@@ -7,5 +7,4 @@
 set -x
 git filter-branch -f --msg-filter  'sed -e "s%ticket:\([0-9]*\)[ :]*\(.*\)$%\2\\
 \\
-X-Trac-URL: https://ssl.icu-project.org/trac/ticket/\1\\
-%g"'  -- --all
+X-Ticket-URL: https://ssl.icu-project.org/trac/ticket/\1%g"'  -- --all
--- scripts/icu-git.sh
+++ scripts/icu-git.sh
@@ -1,115 +1,11 @@
 #!/bin/bash
+# © 2016 and later: Unicode, Inc. and others.
+# License & terms of use: http://www.unicode.org/copyright.html
 # Copyright (C) 2015, International Business Machines Corporation and Others. All Rights Reserved.
 # This file is part of the ICU project. http://icu-project.org
 
 # Convert ICU to Git.
 
-ME=${0}
-
-# What are we going to work on? ICU of course.
-REPONAME=icu
-# where are repos stored?  (.gitignored )
-REPODIR=./repos
-# public URL to repo
-XREPO=http://source.icu-project.org/repos/${REPONAME}
-# rewrite the root to be the public URL
-TOOLDIR=`dirname $ME`
-COMMON_OPTS="--authors-file=../${TOOLDIR}/authors.txt --prefix=origin/ --rewrite-root=${XREPO}/"
-#BASEDIR=${TOOLDIR}/git
-BASEDIR=./git
-VERB="clone -q"
-#--preserve-empty-dirs"
-
-# Which subrepos to do? icu4jni is smaller..
-SUBS=
-#SUBS+="tools "
-#SUBS+="icuhtml "
-#SUBS+="icu4jni "
-SUBS+="icu4j "
-#SUBS+="icu "
-#SUBS+="data "
-#SUBS+="icuapps "
-
-
-if [ ! -d ${REPODIR} ]; then
-    echo ${ME}: "No dir: ${REPODIR} - check REPODIR"
-    exit 1
-fi
-
-
-if [ ! -d ${REPODIR}/${REPONAME} ]; then
-    echo ${ME}: "No dir: ${REPODIR}/${REPONAME} - check REPONAME"
-    exit 1
-fi
-# URL to repo
-REPO=file://`cd  ${REPODIR}/${REPONAME}&&pwd`
-
-# sanity check. Save hours of processing later
-echo "# Fetching all authors from ${REPO} ..."
-svn log --quiet "${REPO}" | grep "^r" | awk '{print $3}' | sort | uniq > scripts/authors-all.txt
-cut -d' ' -f1 < scripts/authors.txt | sort > scripts/authors-configured.txt
-echo "# Making sure scripts/authors.txt is up to date. List of users NOT in scripts/authors.txt:"
-
-if fgrep -v -f scripts/authors-configured.txt scripts/authors-all.txt;
-then
-    echo "^^ please add these to authors.txt and try again."
-    exit 1
-else
-    echo "# -- OK! You may proceed."
-fi
-
-
-#$ fgrep -v -f authors-configured.txt authors-all.txt 
-#scott
-#! srl@drawbridge:~/src/icu-git/scripts
-#$ fgrep -v -f authors-configured.txt ^C
-#(130) ! srl@drawbridge:~/src/icu-git/scripts
-#
-
-
-
-if [ ! -d "${BASEDIR}" ]; then
-    mkdir -vp "${BASEDIR}"
-fi
-
-cd "${BASEDIR}" || exit 1
-
-gitfilter()
-{
-    ../${TOOLDIR}/gitfilter.sh
-}
-
-dosub()
-{
-    if [[ $# != 1 ]]; then
-        echo 'usage: dosub <svnpath>'
-        exit 1
-    fi
-    SUB=$1
-    if [[ -d "${SUB}" ]]; then
-        echo "Already a dir: ${SUB}"
-        exit 1
-    fi
-    echo "# ${SUB}"
-    set -o xtrace
-    git svn ${VERB} ${COMMON_OPTS} "${REPO}" "${SUB}" -T "${SUB}/trunk"  -t "${SUB}/tags" -b "${SUB}/branches" --ignore-paths="^/icu4j/(?:trunk|tags/.*|branches/.*)/main/shared/data" || exit 1
-    ( cd "${SUB}" && ../../${TOOLDIR}/gitfilter.sh ) || exit 1
-    #git remote add github "git@github.com:icu-project/${SUB}.git"
-    set +o xtrace
-}
-
-echo "BASEDIR=${BASEDIR}"
-echo "REPO=${REPO}"
-
-for sub in ${SUBS}; do
-    dosub "${sub}"
-    # should have done this a long time ago. Maybe 2006.
-    if [ -d icu -a ! -d icu4c ]; then
-	mv -v icu icu4c
-    fi
-    if [ -d tools -a ! -d icu-tools ]; then
-	mv -v tools icu-tools
-    fi
-done
-
+set -x
+subgit install --rebuild repos/icu/ && (cd git/icu.git && sh ../../scripts/gitfilter.sh )
 
