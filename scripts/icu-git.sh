f68f2567eade6def20bfd2c2aed36beee944bb90
Jeff Genovy <jefgen@microsoft.com>
2018-07-09 15:07:39 -0700

    Update subgit.conf to ignore the "ignore-me" branch. Also update the conversion script to better document what was used.

--- scripts/icu-git.sh
+++ scripts/icu-git.sh
@@ -14,6 +14,19 @@ fi
 set -x
 git init --bare git/icu.git
 subgit configure --layout auto file://$(pwd)/repos/icubis git/icu.git
-mv -v git/icu.git/subgit/config subgit.conf~ ; cp -v subgit.conf git/icu.git/subgit/config
+mv -v git/icu.git/subgit/config subgit.conf~
+cp -v subgit.conf git/icu.git/subgit/config
 subgit install --rebuild git/icu.git || exit 1
+
 # && (cd git/icu.git && sh ../../scripts/gitfilter.sh )
+
+## If successful, then we no longer need SubGit
+# subgit uninstall --purge git/icu.git
+## clean up anything leftover from the filter-branch.
+git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin ; git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+
+## Migrate select file types to LFS. (this takes awhile).
+git lfs migrate import --everything --include="*.jar,*.dat,*.zip,*.gz,*.bz2,*.gif"
+
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
